7b00cf3642434c630f16d85ee65279907edcd45d
harsha <harsha@harsha-Lenovo-U410.(none)>
2014-04-08 16:58:51 -0400

    Home, Camera, FB Login

---
+++ RateMyDress/src/com/innovators/ratemydress/UserDetailsActivity.java
@@ -0,0 +1,202 @@
+package com.innovators.ratemydress;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.facebook.FacebookRequestError;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.ProfilePictureView;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class UserDetailsActivity extends Activity {
+
+	private ProfilePictureView userProfilePictureView;
+	private TextView userNameView;
+	private TextView userLocationView;
+	private TextView userGenderView;
+	private TextView userDateOfBirthView;
+	private TextView userRelationshipView;
+	
+	public static final String TAG = UserDetailsActivity.class.getSimpleName();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_user_details);
+		
+		userProfilePictureView = (ProfilePictureView) findViewById(R.id.userProfilePicture);
+		userNameView = (TextView) findViewById(R.id.userName);
+		userLocationView = (TextView) findViewById(R.id.userLocation);
+		userGenderView = (TextView) findViewById(R.id.userGender);
+		userDateOfBirthView = (TextView) findViewById(R.id.userDateOfBirth);
+		userRelationshipView = (TextView) findViewById(R.id.userRelationship);
+
+		// Fetch Facebook user info if the session is active
+		Session session = ParseFacebookUtils.getSession();
+		if (session != null && session.isOpened()) {
+		makeMeRequest();
+		}
+	}
+	
+	private void makeMeRequest() {
+		Request request = Request.newMeRequest(ParseFacebookUtils.getSession(),
+				new Request.GraphUserCallback() {
+					@Override
+					public void onCompleted(GraphUser user, Response response) {
+						if (user != null) {
+							// Create a JSON object to hold the profile info
+							JSONObject userProfile = new JSONObject();
+							try {
+								// Populate the JSON object
+								userProfile.put("facebookId", user.getId());
+								userProfile.put("name", user.getName());
+								if (user.getLocation().getProperty("name") != null) {
+									userProfile.put("location", (String) user
+											.getLocation().getProperty("name"));
+								}
+								if (user.getProperty("gender") != null) {
+									userProfile.put("gender",
+											(String) user.getProperty("gender"));
+								}
+								if (user.getBirthday() != null) {
+									userProfile.put("birthday",
+											user.getBirthday());
+								}
+								if (user.getProperty("relationship_status") != null) {
+									userProfile.put("relationship_status",(String) user
+											.getProperty("relationship_status"));
+								}
+
+								// Save the user profile info in a user property
+								ParseUser currentUser = ParseUser
+										.getCurrentUser();
+								currentUser.put("profile", userProfile);
+								currentUser.saveInBackground();
+								
+								// Show the user info
+								updateViewsWithProfileInfo();
+							} catch (JSONException e) {
+								Log.d(TAG,"Error parsing returned user data.");
+							}
+
+						} else if (response.getError() != null) {
+							if ((response.getError().getCategory() == FacebookRequestError.Category.AUTHENTICATION_RETRY)
+									|| (response.getError().getCategory() == FacebookRequestError.Category.AUTHENTICATION_REOPEN_SESSION)) {
+								Log.d(TAG,"The facebook session was invalidated.");
+//								onLogoutButtonClicked();
+							} else {
+								Log.d(TAG,"Some other error: "
+												+ response.getError()
+												.getErrorMessage());
+							}
+						}
+
+					}
+				});
+			request.executeAsync();
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if (currentUser != null) {
+			// Check if the user is currently logged
+			// and show any cached content
+			updateViewsWithProfileInfo();
+		} else {
+			// If the user is not logged in, go to the
+			// activity showing the login view.
+			startLoginActivity();
+		}
+	}
+
+	
+	
+	private void startLoginActivity() {
+		Intent intent = new Intent(this, LoginActivity.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(intent);		
+	}
+
+	private void updateViewsWithProfileInfo() {
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if (currentUser.get("profile") != null) {
+			JSONObject userProfile = currentUser.getJSONObject("profile");
+			try {
+				if (userProfile.getString("facebookId") != null) {
+					String facebookId = userProfile.get("facebookId")
+							.toString();
+					userProfilePictureView.setProfileId(facebookId);
+				} else {
+					// Show the default, blank user profile picture
+					userProfilePictureView.setProfileId(null);
+				}
+				if (userProfile.getString("name") != null) {
+					userNameView.setText(userProfile.getString("name"));
+				} else {
+					userNameView.setText("");
+				}
+				if (userProfile.getString("location") != null) {
+					userLocationView.setText(userProfile.getString("location"));
+				} else {
+					userLocationView.setText("");
+				}
+				if (userProfile.getString("gender") != null) {
+					userGenderView.setText(userProfile.getString("gender"));
+				} else {
+					userGenderView.setText("");
+				}
+				if (userProfile.getString("birthday") != null) {
+					userDateOfBirthView.setText(userProfile
+							.getString("birthday"));
+				} else {
+					userDateOfBirthView.setText("");
+				}
+				if (userProfile.getString("relationship_status") != null) {
+					userRelationshipView.setText(userProfile
+							.getString("relationship_status"));
+				} else {
+					userRelationshipView.setText("");
+				}
+			} catch (JSONException e) {
+				Log.d(TAG,"Error parsing saved user data.");
+			}
+
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.user_details, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+		
+}
